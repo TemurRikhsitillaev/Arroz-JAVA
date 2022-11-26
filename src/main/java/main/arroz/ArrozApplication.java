d84c2b3a6fc0d92c55257808bcf91b9595c464e9
AgileStoner <91374094+AgileStoner@users.noreply.github.com>
2022-11-26 21:46:24 +0500

    AgileStoner Admin

--- src/main/java/main/arroz/ArrozApplication.java
+++ src/main/java/main/arroz/ArrozApplication.java
@@ -2,16 +2,20 @@ package main.arroz;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class ArrozApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ArrozApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ArrozApplication.class.getResource("admin-scene.fxml")); //git add . //git commit -m "Any" //git push
         Scene scene = new Scene(fxmlLoader.load());
+        //String css = this.getClass().getResource("styles.css").toExternalForm();
+        //scene.getStylesheets().add(css);
         stage.setTitle("Arroz");
         stage.setScene(scene);
         stage.show();
